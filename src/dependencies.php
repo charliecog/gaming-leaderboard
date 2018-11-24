90784012a0917735f5e85328bbf3283799256d02
charliecog <charlie.coggans@mayden.co.uk>
2018-11-24 11:23:31 +0000

    added db creds to the dic and settings

--- src/dependencies.php
+++ src/dependencies.php
@@ -9,11 +9,8 @@ $container['renderer'] = function ($c) {
     return new Slim\Views\PhpRenderer($settings['template_path']);
 };
 
-// monolog
-$container['logger'] = function ($c) {
-    $settings = $c->get('settings')['logger'];
-    $logger = new Monolog\Logger($settings['name']);
-    $logger->pushProcessor(new Monolog\Processor\UidProcessor());
-    $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
-    return $logger;
+$container['dbConnection'] = function ($c) {
+    $settings = $c->get('settings')['db'];
+    $db = new PDO($settings['host'].$settings['dbName'], $settings['userName']);
+    return $db;
 };
--- src/settings.php
+++ src/settings.php
@@ -9,11 +9,10 @@ return [
             'template_path' => __DIR__ . '/../templates/',
         ],
 
-        // Monolog settings
-        'logger' => [
-            'name' => 'slim-app',
-            'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
-            'level' => \Monolog\Logger::DEBUG,
-        ],
+        'db' => [
+            'host' => 'mysql:host=192.168.20.20;',
+            'dbName' =>'dbname=gaming_score',
+            'userName' =>'root',
+        ]
     ],
 ];
